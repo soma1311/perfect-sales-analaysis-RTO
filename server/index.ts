62b2db7ba62ac660aa3abfc5a17a66d32a4bc52b
soma1311 <87514136+soma1311@users.noreply.github.com>
2025-07-10 17:45:33 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,8 @@ import { registerRoutes } from "./routes";
 import { log, serveStatic, setupVite } from "./vite";
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use((req, res, next) => {
   const start = Date.now();
